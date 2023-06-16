8556eb8509381735c23195b6c9550633175400f5
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 14:34:30 +0200

    Update Serdes.d.ts

--- lib/Serdes.d.ts
+++ lib/Serdes.d.ts
@@ -3,15 +3,16 @@ export type Serdes<I, O> = {
 	Des(Value: O): I;
 };
 
-export namespace SerdesModule {
+declare namespace SerdesModule {
 	const Any: Serdes<unknown, unknown>;
 	const Integer: Serdes<number, string>;
 	const Number: Serdes<number, string | number>;
 
 	function List<I, O>(Serdes: Serdes<I, O>): Serdes<I[], O[]>;
-	function Interface(Interface: {
-		[key: string]: Serdes<unknown, unknown>;
-	}): Serdes<{ [key: string]: unknown }, unknown[]>;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function Interface<T extends { [key: string]: Serdes<unknown, unknown> }>(Interface: T): Serdes<T, LuaTuple<any>>;
 	function Ser<I, O>(Serdes: Serdes<I, O>): (Value: I) => O;
 	function Des<I, O>(Serdes: Serdes<I, O>): (Value: O) => I;
 }
+
+export default SerdesModule;
